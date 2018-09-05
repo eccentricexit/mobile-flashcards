fdce56bab69c04a912f5b2cc28b245c11c7736be
Matheus Faria de Alencar <mtsalenc@gmail.com>
2018-09-05 16:38:41 -0300

    feat: Styled quiz view

--- components/Quiz.js
+++ components/Quiz.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react'
-import { Text, View, Button } from 'react-native'
 import { connect } from 'react-redux'
 import { updateNotif } from '../utils'
-import { primary, white } from '../utils/colors'
+import { primary, danger, success, white, primaryLight } from '../utils/colors'
+import { 
+  TouchableOpacity,
+  View,
+  Text,
+  StyleSheet,
+  Platform
+} from 'react-native'
 
 class Quiz extends Component {
   static navigationOptions = {
@@ -13,6 +19,7 @@ class Quiz extends Component {
     headerStyle: {
       backgroundColor: primary
     },
+    headerTintColor: white
   }
 
   state = {
@@ -75,8 +82,12 @@ class Quiz extends Component {
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
           <Text>Game over</Text>
           <Text>You got {`${score}% correct answers.`}</Text>
-          <Button title='Restart Quiz' onPress={() => this.handleStartOver()} />
-          <Button title='Back to Deck' onPress={() => this.handleBack()} />
+          <TouchableOpacity onPress={() => this.handleStartOver()}>
+            <Text>Restart Quiz</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => this.handleBack()}>
+            <Text>Back to Deck</Text>
+          </TouchableOpacity>
         </View>
       )
     }
@@ -84,17 +95,80 @@ class Quiz extends Component {
     const currQuestion = deck.questions[currQuestionPos]
     
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>{`${currQuestionPos}/${totalNumQuestions}`}</Text>
-        <Text>{currQuestion.question}</Text>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>        
+        <Text style={styles.title}>{currQuestion.question}</Text>
+        <Text style={styles.textMuted}>{`${currQuestionPos}/${totalNumQuestions}`}</Text>
         {this.state.showAnswer && <Text>{currQuestion.answer}</Text>}
-        <Button title='Yes' onPress={() => this.handleSubmitAnswer('yes')} />
-        <Button title='No' onPress={() => this.handleSubmitAnswer('no')} />
-        <Button title='Show answer' onPress={() => this.handleShowAnswer()} />
+        <TouchableOpacity 
+          onPress={() => this.handleSubmitAnswer('yes')}
+          style={[Platform.OS === 'ios' 
+            ? styles.iosSubmitBtn 
+            : styles.androidPrimaryBtn
+          ,{backgroundColor: success}]}
+        >
+          <Text style={styles.textBtn}>Yes</Text>
+        </TouchableOpacity>        
+        <TouchableOpacity 
+          onPress={() => this.handleSubmitAnswer('no')}
+          style={[Platform.OS === 'ios' 
+            ? styles.iosSubmitBtn 
+            : styles.androidPrimaryBtn
+          ,{backgroundColor: danger}]}
+        >
+          <Text style={styles.textBtn}>No</Text>
+        </TouchableOpacity>
+        <TouchableOpacity 
+          onPress={() => this.handleShowAnswer()}
+          style={[Platform.OS === 'ios' 
+            ? styles.iosSubmitBtn 
+            : styles.androidPrimaryBtn
+          ,{marginTop: 28}]}
+        >
+          <Text style={styles.textBtn}>Show answer</Text>
+        </TouchableOpacity>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  iosSubmitBtn: {
+    backgroundColor: primary,
+    padding: 10,
+    borderRadius: 7,
+    height: 45,    
+  },
+  androidPrimaryBtn: {
+    backgroundColor: primary,
+    padding: 10,
+    paddingLeft: 30,
+    paddingRight: 30,
+    width: 250,
+    height: 65,
+    borderRadius: 4,
+    justifyContent: 'center',
+    alignItems: 'stretch',
+    elevation:2,
+    marginTop:8
+  },
+  title: {
+    fontSize: 42,    
+  },
+  textBtn: {    
+    fontSize:20,
+    color: white,
+    textAlign: 'center'
+  },
+  textMuted: {
+    fontSize: 22,    
+    marginBottom:120,
+    textAlign: 'center',
+    color: primaryLight
+  }
+})
 
 function mapStateToProps ({ state }) {
   return { state }
--- utils/colors.js
+++ utils/colors.js
@@ -4,3 +4,6 @@ export const primaryDark = '#000a12'
 export const primaryText = '#ffffff'
 export const black = '#000000'
 export const white = '#ffffff'
+
+export const danger = '#f44336'
+export const success = '#4caf50'
